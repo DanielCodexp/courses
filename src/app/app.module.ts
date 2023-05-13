fe0ff315ba0cc5383917e43cfd65037f3f61a0a3
Daniel Hernandez <angeldanielcode@gmail.com>
2023-05-13 16:58:41 -0600

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { MaterialModule } from 'src/app/material.module';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  exports:[
+    HomeComponent
+  ],
+
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    MaterialModule
+  ]
+})
+export class HomeModule { }
